6c71fe0638d35483751a106c2a67ccca7f1bbe9a
saritasubhra <saritasubhra1@gmail.com>
2025-12-09 22:33:10 +0530

    url changes4

--- app/routes/webhooks.compliance.jsx
+++ app/routes/webhooks.compliance.jsx
@@ -1,4 +1,4 @@
-import { authenticate } from "../../shopify.server";
+import { authenticate } from "../shopify.server";
 
 export const action = async ({ request }) => {
   const { shop, topic } = await authenticate.webhook(request);
