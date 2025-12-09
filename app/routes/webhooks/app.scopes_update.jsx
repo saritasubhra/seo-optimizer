d60e14b746f4b4f4451b96f36b148d4bd197eb29
saritasubhra <saritasubhra1@gmail.com>
2025-12-09 21:26:42 +0530

    url changes3

--- app/routes/webhooks/app.scopes_update.jsx
+++ app/routes/webhooks/app.scopes_update.jsx
@@ -1,4 +1,4 @@
-import { authenticate } from "../shopify.server";
+import { authenticate } from "../../shopify.server";
 import prisma from "../db.server";
 
 export const action = async ({ request }) => {
